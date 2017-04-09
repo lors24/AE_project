768c5e6a291d17b87e9dbde8ed9b16ab85e5c7c7
Lore <lors2404@gmail.com>
2017-04-09 18:12:19 -0400

    clean directory and first model

--- exploratory.R
+++ exploratory.R
@@ -5,6 +5,7 @@ setwd("~/Dropbox (MIT)/MIT/Analytics Edge/Project")
 #Libraries
 
 library(dplyr)
+library(chron)
 
 
 #Read data
@@ -28,10 +29,39 @@ scores <- scores %>%
            Percent.Asian = as.numeric(gsub("%","",Percent.Asian)),
            Percent.Tested = as.numeric(gsub("%","",Percent.Tested)),
            aux = Percent.White + Percent.Black + Percent.Hispanic + Percent.Asian,
-           Percent.Other = pmax(100-aux,0)) %>%
-    select(-c(Phone.Number, State, Start.Time, End.Time,aux))
+           Percent.Other = pmax(100-aux,0),
+           Start.Time = times(paste0(gsub(" AM", '', Start.Time),":00")),
+           End.Time = times(paste0(gsub(" PM", '', End.Time),":00"))+.5,
+           Length.Aux = End.Time-Start.Time,
+           Length = round(hours(scores2$Length)+minutes(scores2$Length)/60,2)) %>%
+    select(-c(Phone.Number, State, aux, Start.Time, End.Time, Length.Aux))
 
 sum(complete.cases(scores))
 
 #trial <- scores %>%
     #left_join(directory, by = c("School.ID" = "dbn"))
+#table(trial$boro, trial$Borough)
+dif <- subset(trial, School.Name != school_name ) %>%
+    select(School.Name, school_name)
+
+
+#borough has same information as Borough in scores
+#school names are identical for 338 records, the 97 remaining differ in capitalization, using articles, or other minor differences
+#zip is identical for all cases
+#se_services equal for all observations -> "This school will provide students with disabilities the supports and services indicated on their IEPs."
+
+
+directory2 <- directory %>%
+    mutate(School.ID = dbn,
+           accesible = ifelse(school_accessibility_description == "Functionally Accessible",1,0)
+           ) %>%
+    select(-c(boro, school_name, building_code, phone_number, fax_number,
+              expgrade_span_min, expgrade_span_max, state_code, zip, city, school_accessibility_description,
+              se_services))
+
+
+#First model
+
+m <- lm(Average.Score..SAT.Math. ~ Percent.Black + Percent.Hispanic + Percent.Asian + Percent.White + Length, data = scores)
+
+#.6692 accuracy
